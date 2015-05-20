a8d007bd858d59346d1b4f775379253222794b83
Derwin McGeary <derwinmcgeary+web@gmail.com>
2015-05-20 20:43:24 +0500

    Working, albeit with some NA warnings

--- best.R
+++ best.R
@@ -5,10 +5,29 @@ best <- function(state, outcome) {
   ## Check that state and outcome are valid
   if(!(state %in% outcomes$State)) { stop("invalid state")}
   if(!(outcome %in% c("heart attack","heart failure","pneumonia"))){ stop("invalid outcome")}
-    
+
+  ## variable "cause" is a column number in a table we construct later
+  ## there must be a better way...
+  if(outcome == "heart attack") {cause <- 2}
+  if(outcome == "heart failure") {cause <- 3}
+  if(outcome == "pneumonia") {cause <- 4}
   ## Return hospital name in that state with lowest 30-day death
   ## rate
   statedata <- subset(outcomes,State == state)
   stateoutcomes <- cbind(statedata[,2],statedata[,11],statedata[,17],statedata[,23])
+
+  colnames(stateoutcomes) <- c("Hospital","heart attack", "heart failure", "pneumonia")
+
   
+  
+  # Now we have a nice matrix with the data we care about
+  # We need to find the minimum value, discarding NAs ...
+  minharm <- range(as.numeric(stateoutcomes[,cause]), na.rm=TRUE)[1]
+
+  # ... and put all rows with that value into bests
+  bests <- as.numeric(stateoutcomes[,cause]) == minharm
+  bests <- which(as.logical(bests))
+
+  # ... and return stateoutcomes[bests,1]
+  stateoutcomes[[bests,1]]
 }
